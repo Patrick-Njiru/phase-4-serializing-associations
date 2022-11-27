1ce552df32372d544441dd24e654337590eef14b
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-11-27 09:40:18 +0300

    Completed assignment.

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,4 +1,6 @@
 class MovieSerializer < ActiveModel::Serializer
   attributes :id, :title, :year, :length, :description, :poster_url, :category, :discount
-  
+  belongs_to :director
+  has_many :reviews
+  # only add the necessary macros to speed up the retreival of data
 end
